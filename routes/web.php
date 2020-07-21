4e130cc3d0eccf73aa06bae0375378167586c962
Andres Morales <andresmaomorales@gmail.com>
2020-07-20 20:18:28 -0500

    Remove routes.

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('register/login');
-})->name('login');
-
-Route::get('/signup', function () {
-    return view('register/signup');
-})->name('signup');
-
-Route::get('/reset_password', function () {
-    return view('register/password/reset');
-})->name('reset_password');
-
-Route::get('/reset_password_email', function () {
-    return view('register/password/email');
-})->name('reset_password_email');
-
 Route::get('/{restaurant}/{branch}', 'MenuController@show')->name('menu');
 Auth::routes();
 
