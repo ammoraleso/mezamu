36b127abe22950b07c5ebd429507527aaebfa3b4
Andres Morales <andresmaomorales@gmail.com>
2020-07-31 21:38:37 -0500

    fix menu without token.

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -143,8 +143,9 @@
             @endfor
         </div>
     </div>
-
-    <div id="back" class="pt-3"style="justify-content: space-evenly; display: flex; padding-bottom: 1%;">
-        <a class="btn btn-success" href="{{route('cart')}}">{{__('general.GoToCart')}}</a>
-    </div>
+    @if ($allowAdd)
+        <div id="back" class="pt-3"style="justify-content: space-evenly; display: flex; padding-bottom: 1%;">
+            <a class="btn btn-success" href="{{route('cart')}}">{{__('general.GoToCart')}}</a>
+        </div>
+    @endif
 @endsection
