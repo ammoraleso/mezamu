6719eb1caaf8b51aaa57b709e900442eb90c0b5e
c.hernandez <camehernandezcas@unal.edu.co>
2020-08-06 15:42:23 -0500

    commit to stash

--- public/js/app.js
+++ public/js/app.js
@@ -49888,8 +49888,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Andres Morales\Desktop\Andres\PHP\mezamu\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Andres Morales\Desktop\Andres\PHP\mezamu\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\c.hernandez\Documents\mezamu\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\c.hernandez\Documents\mezamu\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
