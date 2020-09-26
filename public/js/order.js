420e666a01c329b19cae668dfed93198059a5967
Andres Morales <andresmaomorales@gmail.com>
2020-09-26 00:29:19 -0500

    Add aditional details to address

--- public/js/order.js
+++ public/js/order.js
@@ -31,23 +31,6 @@ async function loadOrders() {
 }
 
 function myFunction() {
-    // var input, filter, table, tr, td, i, txtValue;
-    // input = document.getElementById("myInput");
-    // filter = input.value.toUpperCase();
-    // table = document.getElementById("myTable");
-    // tr = table.getElementsByTagName("tr");
-    // for (i = 0; i < tr.length; i++) {
-    //     td = tr[i].getElementsByTagName("td")[0];
-    //     if (td) {
-    //         txtValue = td.textContent || td.innerText;
-    //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    //             tr[i].style.display = "";
-    //         } else {
-    //             tr[i].style.display = "none";
-    //         }
-    //     }
-    // }
-
     var input, filter, table, tr, td, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -71,4 +54,23 @@ function myFunction() {
             }
         }
     }
+}
+
+function showDetails(customerToDetail) {
+    document.getElementById("header-tittle").innerHTML =
+        customerToDetail.nombre;
+
+    document.getElementById("email").innerHTML =
+        "<strong>Email: </strong>" + customerToDetail.email;
+
+    document.getElementById("phone").innerHTML =
+        "<strong>Telefono: </strong>" + customerToDetail.telefono;
+
+    document.getElementById("address").innerHTML =
+        "<strong>Dirección: </strong>" + customerToDetail.direccion;
+
+    document.getElementById("address_add").innerHTML =
+        "<strong>Adicion Dirección: </strong>" +
+        customerToDetail.direccion_adicional;
+    $("#modalDetailsCustomer").modal("show");
 }
---
+++ resources/views/modalDetailsCustomer.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="modalDetailsCustomer" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title m-auto text-center modal-header" id="header-tittle"></h5>
+                <button type="button" class="close m-0 p-0 modal-close-button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <p id="email" style="padding-left: 3%; padding-top: 6%; margin-bottom: 0px;"><p>
+            <p id="phone" style="padding-left: 3%; padding-top: 6%; margin-bottom: 0px;"><p>
+            <p id="address" style="padding-left: 3%; padding-top: 6%; margin-bottom: 0px;"><p>
+            <p id="address_add" style="padding-left: 3%; padding-top: 6%; margin-bottom: 0px;"><p>
+        </div>
+    </div>
+</div>
+
+
