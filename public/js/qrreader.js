8adad62f28e9f4b6bfc327b60929a9ef41c80702
Andres Morales <andresmaomorales@gmail.com>
2020-10-30 17:32:24 -0500

    rollback last change

--- public/js/qrreader.js
+++ public/js/qrreader.js
@@ -1,6 +1,6 @@
 let continueScanning = true;
 async function onScanSuccess(qrMessage) {
-    if (continueScanning) {
+    if(continueScanning) {
         continueScanning = false;
         try {
             await $.ajax({
@@ -10,16 +10,15 @@ async function onScanSuccess(qrMessage) {
                 url: checkOutUrl,
                 data: {
                     token: qrMessage,
-                    total: document.getElementById("totalPrice").value,
-                    description: document.getElementById("descriptionOrder")
-                        .value
+                    total: document.getElementById('totalPrice').value,
+                    description: document.getElementById("descriptionOrder").value,
                 },
                 type: "post"
             });
         } catch (error) {
             console.log("Error goCheckout put headers" + error);
             continueScanning = true;
-            return;
+            return
         }
         window.location.replace("successfulPurchase");
     }
@@ -32,13 +31,3 @@ function onScanFailure(error) {
 let html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", { fps: 2, qrbox: 250 }, /* verbose= */ true);//2 fps to avoid multiple call onScanSuccess
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-
-const html5QrCode = new Html5Qrcode("#reader");
-const qrCodeSuccessCallback = message => {
-    /* handle success */
-};
-const config = { fps: 10, qrbox: 250 };
-
-// If you want to prefer back camera
-html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
-
